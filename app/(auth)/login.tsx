6403ba2fdf4681fa19e3a91cbeaee6b7f7032592
Andrea Fan <shan32157@gmail.com>
2025-03-09 12:39:21 +0800

    Working on auth

---
+++ app/(auth)/login.tsx
@@ -0,0 +1,118 @@
+import React, { useState } from "react";
+import {
+  Alert,
+  StyleSheet,
+  View,
+  AppState,
+  TextInput,
+  Button,
+} from "react-native";
+import { supabase } from "@/lib/supabase";
+
+AppState.addEventListener("change", (state) => {
+  if (state === "active") {
+    supabase.auth.startAutoRefresh();
+  } else {
+    supabase.auth.stopAutoRefresh();
+  }
+});
+
+export default function Auth() {
+  const [userInfo, setUserInfo] = useState({
+    email: "",
+    password: "",
+    displayName: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  async function signInWithEmail() {
+    setLoading(true);
+    const { error } = await supabase.auth.signInWithPassword({
+      email: userInfo.email,
+      displayName: userInfo.displayName,
+      password: userInfo.password,
+    });
+
+    if (error) Alert.alert(error.message);
+    setLoading(false);
+  }
+
+  async function signUpWithEmail() {
+    setLoading(true);
+    const {
+      data: { session },
+      error,
+    } = await supabase.auth.signUp({
+      email: userInfo.email,
+      displayName: userInfo.displayName,
+      password: userInfo.password,
+    });
+
+    if (error) Alert.alert(error.message);
+    if (!session)
+      Alert.alert("Please check your inbox for email verification!");
+    setLoading(false);
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={[styles.verticallySpaced, styles.mt20]}>
+        <TextInput
+          onChangeText={(text) =>
+            setUserInfo({ ...userInfo, displayName: text })
+          }
+          value={userInfo.displayName}
+          placeholder="Please enter your display name"
+          autoCapitalize={"none"}
+        />
+      </View>
+      <View style={[styles.verticallySpaced, styles.mt20]}>
+        <TextInput
+          onChangeText={(text) => setUserInfo({ ...userInfo, email: text })}
+          value={userInfo.email}
+          placeholder="email@address.com"
+          autoCapitalize={"none"}
+        />
+      </View>
+      <View style={styles.verticallySpaced}>
+        <TextInput
+          onChangeText={(text) => setUserInfo({ ...userInfo, password: text })}
+          value={userInfo.password}
+          secureTextEntry={true}
+          placeholder="Password"
+          autoCapitalize={"none"}
+        />
+      </View>
+      <View style={[styles.verticallySpaced, styles.mt20]}>
+        <Button
+          title="Sign in"
+          disabled={loading}
+          onPress={() => signInWithEmail()}
+        />
+      </View>
+      <View style={styles.verticallySpaced}>
+        <Button
+          title="Sign up"
+          disabled={loading}
+          onPress={() => signUpWithEmail()}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+    padding: 12,
+  },
+  verticallySpaced: {
+    paddingTop: 4,
+    paddingBottom: 4,
+    alignSelf: "stretch",
+  },
+  mt20: {
+    marginTop: 20,
+  },
+});
